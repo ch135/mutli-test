74b9b76d4a3741208bd1b99b16c48dc0bffa89c8
scholarly <707347865@qq.com>
2019-07-22 07:23:11 +0800

    视频抽取图像

--- test.py
+++ test.py
@@ -10,6 +10,7 @@ import cv2
 import os
 import numpy as np
 
+
 # ########### 摄像头测试 ##############
 # video = cv2.VideoCapture(0)
 # while True:
@@ -41,20 +42,53 @@ import numpy as np
 #     file.write("\n")
 
 # ########### 图像合成视频测试 ##############
-def Video(path, size):
-    files = os.listdir(path)
-    fps = 25
-    v_path = "./basketball.mp4"
-    fourcc = cv2.VideoWriter_fourcc("D", "I", "V", "X")
+# def Video(path, size):
+#     files = os.listdir(path)
+#     fps = 25
+#     v_path = "./basketball.mp4"
+#     fourcc = cv2.VideoWriter_fourcc("D", "I", "V", "X")
+#
+#     video = cv2.VideoWriter(v_path, fourcc, fps, size)
+#
+#     for item in files:
+#         if item.endswith(".jpg"):
+#             item = path + "/" + item
+#             img = cv2.imread(item)
+#             video.write(img)
+#
+#     video.release()
+#
+#
+# Video("E:/haochen/dataset/OTB100/basketball", (576, 432))
 
-    video = cv2.VideoWriter(v_path, fourcc, fps, size)
+# # ########### 视频抽取图像测试 ##############
+import os
+import cv2
 
-    for item in files:
-        if item.endswith(".jpg"):
-            item = path+"/"+item
-            img = cv2.imread(item)
-            video.write(img)
 
-    video.release()
+def videoToimg(video_path, img_path):
+    videos = os.listdir(video_path)
+    for video in videos:
+        name, _ = video.split('.')
+        vpath = os.path.join(video_path, video)
+        impath = os.path.join(img_path, video.split('.')[0])
 
-Video("E:/haochen/dataset/OTB100/basketball", (576, 432))
+        if not os.path.exists(impath):
+            os.makedirs(impath)
+
+        cap = cv2.VideoCapture(vpath)
+        frame_count = 1
+        success = True
+        while (success):
+            success, frame = cap.read()
+
+            params = []
+            params.append(1)
+            if (success):
+                cv2.imwrite(impath + "/%d.jpg" % frame_count, frame, params)
+            frame_count += 1
+    cap.release()
+
+
+if __name__ == '__main__':
+    videoToimg("E:/haochen/dataset/track", "E:/haochen/dataset/track")
